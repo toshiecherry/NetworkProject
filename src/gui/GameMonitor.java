df73d95731a152981965c4420e96079fc78ac858
Sara59 <dic13sli@student.lu.se>
2015-05-20 15:00:32 +0200

    mojs

--- src/gui/GameMonitor.java
+++ src/gui/GameMonitor.java
@@ -14,10 +14,11 @@ public class GameMonitor {
 		JFrame frame = new JFrame("Bubblan");
 		frame.setName("Bubblan");
 		frame.setBounds(500, 100, 450, 300);
-		String name = JOptionPane.showInputDialog("Ange användarnamn");
+		String name = JOptionPane.showInputDialog("Select username");
 		handler.setPlayerName(name);
 		ButtonAndMessagePanel bottomPanel = new ButtonAndMessagePanel(this);
 		frame.add(bottomPanel, BorderLayout.SOUTH);
+		
 		
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
