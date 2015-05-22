3a54b02d90c68d1a1f270b26e938e8954effb7e7
Sara59 <dic13sli@student.lu.se>
2015-05-22 20:09:13 +0200

    stuff

--- src/gui/ButtonAndMessagePanel.java
+++ src/gui/ButtonAndMessagePanel.java
@@ -1,7 +1,6 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,18 +11,26 @@ import client.ClientSender;
 public class ButtonAndMessagePanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JLabel messageLabel;
+//	private JLabel messageLabel2;
 
 	public ButtonAndMessagePanel(GameMonitor monitor, ClientSender sender, CardHand cardHand) {
-		setLayout(new BorderLayout());
+		setLayout(new BorderLayout()); 
 		messageLabel = new JLabel("       ");
+//		messageLabel2 = new JLabel("       ");
 		add(new DrawCardButton(monitor, sender, cardHand),BorderLayout.WEST);
 		add(new BubblanButton(monitor, sender, cardHand), BorderLayout.EAST);
 		add(messageLabel, BorderLayout.SOUTH);
+//		add(messageLabel2, BorderLayout.NORTH);
 	}
 
 	public void setText(String error) {
 		messageLabel.setText(error);
 
 	}
+//	public void setText2(String error) {
+//		messageLabel2.setText(error);
+//
+//	}
+	
 
 }
